36db5aef5d352b59e30af17e8aaf4c87ed7c6078
Kohakuwu <asakurasayori@gmail.com>
2024-09-26 19:12:22 +0800

    commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import FrontPageComponentSet from "@/components/content-components/frontpage-components";
+import { Page } from "@/components/whole-page-export";
+
+export default function MainPage() {
+
+    return (
+        <>
+            <Page showVideo={false} showBgPictures={true}>
+                <FrontPageComponentSet></FrontPageComponentSet>
+            </Page>
+        </>
+    );
+}
--- components/content-components/intropage-components.tsx
+++ components/content-components/intropage-components.tsx
@@ -1,21 +1,48 @@
+import Image from "next/image"
+import { ReactNode } from "react"
+
 export default function IntroPageComponentSet() {
     return (
-        <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2`">
+        <div className="fixed top-[50vh] left-[50vw] -translate-x-1/2 -translate-y-1/2 ipcs items-center">
             <LeftCard></LeftCard>
+            <RightCard></RightCard>
         </div>
     )
 }
 
 function LeftCard() {
     return (
-        <div className="h-24 w-24">
+        <div className="w-[35vh] h-[35vh] flex justify-center items-center mx-[3vw]">
             <Avatar></Avatar>
+        </div>
+    )
+}
+
+function RightCard() {
+    return (
+        <div>
+            <div className="title w-[50vw] introduction-title text-3xl">INTRODUCTION</div>
+            <IntroInfo></IntroInfo>
         </div>
     )
 }
 
 function Avatar() {
     return (
-        <div className="avatar h-24 w-24"></div>
+        <img className="h-max w-max rounded-full m-5" src="/avatar/ava.jpg"></img>
+    )
+}
+function IntroLine({ children }: { children: ReactNode }) {
+    return <div className="introtext">{children}</div>
+}
+
+function IntroInfo() {
+    return (
+        <div className="IntroInfo">
+            <IntroLine>这里是忆梦，来自上海，大学就读中，对象<div className="text-green-500 inline">@迷途面谈</div>。</IntroLine>
+            <IntroLine>工作重心在Web开发，主要使用Next.JS+TailwindCSS</IntroLine>
+            <IntroLine>主坑：明日方舟/FFXIV/舞萌/オンゲキ/东方非想天则</IntroLine>
+            <IntroLine>マイRating16100 撃Rating16.75 姆Q相对一</IntroLine>
+        </div>
     )
 }
