a064d10899326aeffadad31c6ab36d6a73451557
Kohakuwu <asakurasayori@gmail.com>
2024-10-03 00:55:10 +0800

    commit

--- app/intro/page.tsx
+++ app/intro/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 import IntroPageComponentSet from "@/components/content-components/intropage-components";
 import { Page } from "@/components/whole-page-export";
+import React from "react";
 
 export default function MainPage() {
 
     return (
         <>
-            <Page showVideo={false} showBgPictures={true}>
+            <Page showVideo={false} showBgPictures={true} hoverVisible={false}>
                 <IntroPageComponentSet></IntroPageComponentSet>
             </Page>
         </>
